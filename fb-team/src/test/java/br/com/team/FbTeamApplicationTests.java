c49011e60f613bf5b581f8fb1756b1b33402a36f
hernandessantos <hernandes.santos1@gmail.com>
2021-02-18 20:44:54 -0300

    configurin files

---
+++ fb-team/src/test/java/br/com/team/FbTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
